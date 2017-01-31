c2576f11dd6ee1d1ffe91a4169409303f3a12956
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2017-01-31 04:17:01 +0100

    added first draft

--- goobi-plugin-step-user-assignement/src/de/intranda/goobi/plugins/UserAssignmentPlugin.java
+++ goobi-plugin-step-user-assignement/src/de/intranda/goobi/plugins/UserAssignmentPlugin.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.goobi.beans.Step;
+import org.goobi.beans.User;
+import org.goobi.beans.Usergroup;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.plugin.interfaces.AbstractStepPlugin;
 import org.goobi.production.plugin.interfaces.IPlugin;
@@ -24,6 +26,7 @@ import de.sub.goobi.config.ConfigPlugins;
 import de.sub.goobi.helper.NIOFileUtils;
 import de.sub.goobi.helper.exceptions.DAOException;
 import de.sub.goobi.helper.exceptions.SwapException;
+import de.sub.goobi.samples.BenutzergruppenTest;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
 import lombok.extern.log4j.Log4j;
 
@@ -31,145 +34,83 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class UserAssignmentPlugin extends AbstractStepPlugin implements IStepPlugin, IPlugin {
 
-    private static final String PLUGIN_NAME = "intranda_step_user_assignment";
+	private static final String PLUGIN_NAME = "intranda_step_user_assignment";
+	private Step targetStep;
+	private List<Usergroup> oldGroups;
+	private List<UserWrapper> users;
 
-    private String masterFolder;
-    private Path folder;
+	public void initialize(Step step, String returnPath) {
+		super.returnPath = returnPath;
+		super.myStep = step;
+		oldGroups = new ArrayList<>();
+		users = new ArrayList<UserWrapper>();
 
-    private String allowedTypes;
-    
-    private String currentFile = null;
-    private List<String> uploadedFiles = new ArrayList<String>();
+		// find the right step
+		String targetStepTitle = "Scannen";
+		// collect all users
+		for (Step s : myStep.getProzess().getSchritte()) {
+			if (s.getTitel().equals(targetStepTitle)) {
+				targetStep = s;
+				// get all current usergroups
+				for (Usergroup ug : s.getBenutzergruppen()) {
+					oldGroups.add(ug);
+					for (User u : ug.getBenutzer()) {
+						if (!userExistsInList(u))
+							users.add(new UserWrapper(u, false));
+					}
+				}
+			}
+			// get all current users
+			for (User u : s.getBenutzer()) {
+				if (!userExistsInList(u))
+					users.add(new UserWrapper(u, false));
+			}
+		}
+	}
 
-    public void initialize(Step step, String returnPath) {
-        super.returnPath = returnPath;
-        super.myStep = step;
-        try {
-            masterFolder = myStep.getProzess().getImagesOrigDirectory(false);
-            folder = Paths.get(masterFolder);
-            
-            allowedTypes = ConfigPlugins.getPluginConfig(this).getString("regex", "/(\\.|\\/)(gif|jpe?g|png|tiff?|jp2|pdf)$/");
-            if (!Files.exists(folder)) {
-                Files.createDirectory(folder);
-            }
-            loadUploadedFiles();
+	private boolean userExistsInList(User u) {
+		for (UserWrapper userWrapper : users) {
+			if (userWrapper.getUser().equals(u)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-        } catch (SwapException | DAOException | IOException | InterruptedException e) {
-            log.error(e);
-        }
+	public List<Usergroup> getOldGroups() {
+		return oldGroups;
+	}
 
-    }
+	public List<UserWrapper> getUsers() {
+		return users;
+	}
+	
+	public void toggleUser(UserWrapper uw){
+		uw.setMember(!uw.getMember());
+	}
 
-    private void loadUploadedFiles(){
-        uploadedFiles = NIOFileUtils.list(folder.toString());
-        Collections.sort(uploadedFiles);
-    }
-    
-    @Override
-    public boolean execute() {
-        return false;
-    }
+	@Override public boolean execute() {
+		return false;
+	}
 
-    @Override
-    public PluginGuiType getPluginGuiType() {
-        return PluginGuiType.PART;
-    }
+	@Override
+	public PluginGuiType getPluginGuiType() {
+		return PluginGuiType.PART;
+	}
 
-    @Override
-    public String getPagePath() {
-        return null;
-    }
+	@Override
+	public String getPagePath() {
+		return null;
+	}
 
-    @Override
-    public String getTitle() {
-        return PLUGIN_NAME;
-    }
+	@Override
+	public String getTitle() {
+		return PLUGIN_NAME;
+	}
 
-    @Override
-    public String getDescription() {
-        return PLUGIN_NAME;
-    }
+	@Override
+	public String getDescription() {
+		return PLUGIN_NAME;
+	}
 
-    public void setUploadedFiles(List<String> uploadedFiles) {
-        this.uploadedFiles = uploadedFiles;
-    }
-
-    public List<String> getUploadedFiles() {
-        return uploadedFiles;
-    }
-
-    public String getCurrentFile() {
-        return currentFile;
-    }
-
-    public void setCurrentFile(String currentFile) {
-        this.currentFile = currentFile;
-    }
-
-    public String getAllowedTypes() {
-        return allowedTypes;
-    }
-    
-    public void deleteFile() {
-        // delete file
-        File f = new File(folder.toString(), currentFile);
-        FileUtils.deleteQuietly(f);
-        loadUploadedFiles();
-    }
-    
-    public void deleteAllFiles(){
-        for (String file : uploadedFiles) {
-            File f = new File(folder.toString(), file);
-            FileUtils.deleteQuietly(f);
-        }
-        loadUploadedFiles();
-    }
-
-    public void handleFileUpload(FileUploadEvent event) {
-        try {
-            copyFile(event.getFile().getFileName(), event.getFile().getInputstream());
-
-        } catch (IOException e) {
-            log.error(e);
-        }
-        
-        loadUploadedFiles();
-    }
-
-    public void copyFile(String fileName, InputStream in) {
-        OutputStream out = null;
-
-        try {
-
-            // write the inputStream to a FileOutputStream
-            out = new FileOutputStream(new File(masterFolder, fileName));
-
-            int read = 0;
-            byte[] bytes = new byte[1024];
-
-            while ((read = in.read(bytes)) != -1) {
-                out.write(bytes, 0, read);
-            }
-            out.flush();
-        } catch (IOException e) {
-            log.error(e);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    log.error(e);
-                }
-            }
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException e) {
-                    log.error(e);
-                }
-            }
-
-        }
-
-    }
 }
